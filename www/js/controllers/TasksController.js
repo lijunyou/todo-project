847e246322cc877ed82b00ba2ed530fc53dbebab
lijunyou <141220056@smail.nju.edu.cn>
2017-04-24 00:10:17 +0800

    Add files via upload

--- www/js/controllers/TasksController.js
+++ www/js/controllers/TasksController.js
@@ -1,4 +1,16 @@
 angular.module('starter.controllers')
 .controller('TasksController', function ($rootScope, $scope, Tasks) {
 	$scope.tasks = Tasks.all();
+$scope.getstyle = function(task){
+                if(task.sb == true)
+                        return 'line-through';
+                else
+                        return 'none';
+        };
+        $scope.complete = function(task, index){
+                $scope.tasks.splice(index, 1);
+                task.sb = true;
+                var index;
+                $scope.tasks.splice(index, 0, task);
+        };
 });
